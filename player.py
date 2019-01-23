dd0223712d691b2947599aad658b753d48f29b6f
murai0823 <40621929+murai0823@users.noreply.github.com>
2019-01-23 13:58:11 +0900

    追加タスク1
    a

--- player.py
+++ player.py
@@ -38,6 +38,14 @@ class GamePlayer(Player):
 
             #コマが置けるかの確認/置けるならその位置を返す(タスク名:Playメソッドの作成)
             #追加タスク
+            if(height >= 0 and height < 3 and width >= 0 and width < 3):
+                if(CheckMath(math,height,width,"  ")):
+                    return [height,width]
+                else:
+                    print("そのマスは置けません")
+            else:
+                print("範囲外です")
+
             #②
 
 #コンピュータのクラス
